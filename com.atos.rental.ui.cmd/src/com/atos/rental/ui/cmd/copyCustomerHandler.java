a3582619bfc3efd2fd34f4fc13f7b948ec8f7c83
Olivier MARY <olivier.mary@atos.net>
2014-06-12 14:58:57 +0200

    rajout d'un shortcut

--- com.atos.rental.ui.cmd/src/com/atos/rental/ui/cmd/copyCustomerHandler.java
+++ com.atos.rental.ui.cmd/src/com/atos/rental/ui/cmd/copyCustomerHandler.java
@@ -7,12 +7,16 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.dnd.Clipboard;
 import org.eclipse.swt.dnd.DND;
+import org.eclipse.swt.dnd.ImageTransfer;
 import org.eclipse.swt.dnd.RTFTransfer;
 import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.handlers.HandlerUtil;
 
+import com.atos.rental.ui.IRentalUIConstants;
+import com.atos.rental.ui.RentalUIActivator;
 import com.opcoach.training.rental.Customer;
 
 public class copyCustomerHandler extends AbstractHandler {
@@ -28,14 +32,19 @@ public class copyCustomerHandler extends AbstractHandler {
 			String rtfData = "{\\rtf1\\b\\i ";
 			for (Object obj : isel.toArray()) {
 				Customer cus = (Customer) obj;
-				textData = textData + "\n" + cus.getDisplayName();
-				rtfData = rtfData + "\n" + cus.getDisplayName();
+				textData = textData + cus.getDisplayName() + "\n";
+				rtfData = rtfData + cus.getDisplayName() + "\n";
 			}
 			rtfData = rtfData + "}";
+			Image img = (Image) RentalUIActivator.getDefault()
+					.getImageRegistry().get(IRentalUIConstants.IMG_AGENCY_KEY);
 			TextTransfer textTransfer = TextTransfer.getInstance();
 			RTFTransfer rtfTransfer = RTFTransfer.getInstance();
-			Transfer[] transfers = new Transfer[] { textTransfer, rtfTransfer };
-			Object[] data = new Object[] { textData, rtfData };
+			ImageTransfer imgTransfer = ImageTransfer.getInstance();
+			Transfer[] transfers = new Transfer[] { textTransfer, rtfTransfer,
+					imgTransfer };
+			Object[] data = new Object[] { textData, rtfData,
+					img.getImageData() };
 			clipboard.setContents(data, transfers, DND.CLIPBOARD);
 			clipboard.dispose();
 		}
